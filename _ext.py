4197871ec54860070f7da9dc703ef527010d5176
Alexander Solovyov <alexander@solovyov.net>
2013-03-26 16:55:35 +0200

    some updates

--- _ext.py
+++ _ext.py
@@ -1,19 +1,25 @@
 import os.path as op
+from datetime import datetime, timedelta
 
 import yaml
 from cyrax import events
 
+def names(item):
+    return item.get('names') or [item['name']]
 
 def parse_data(site):
     data = yaml.load(file(op.join(op.dirname(__file__), 'games.yaml')))
     site.clones = []
     site.reimplementations = []
     for item in data:
+        if 'added' in item:
+            item['added'] = datetime.strptime(item['added'], '%Y-%m-%d').date()
+            item['new'] = item['added'] < (datetime.now() - timedelta(days=30))
+
         if 'clones' in item:
-            site.clones.append((item.get('names') or [item['name']],
-                                item['clones']))
+            site.clones.append((names(item), item['clones']))
         if 'reimplementations' in item:
-            site.reimplementations.append((item.get('names') or [item['name']],
+            site.reimplementations.append((names(item),
                                            item['reimplementations']))
 
 
