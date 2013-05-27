10a1d0116a58dbf824e76b094743adaba9ebe754
Alexander Solovyov <alexander@solovyov.net>
2013-05-27 18:36:25 +0300

    fix "new" items

--- _ext.py
+++ _ext.py
@@ -1,5 +1,5 @@
 import os.path as op
-from datetime import datetime, timedelta
+from datetime import date, timedelta
 
 import yaml
 from cyrax import events
@@ -7,20 +7,23 @@ from cyrax import events
 def names(item):
     return item.get('names') or [item['name']]
 
+def mark_new(item):
+    if 'added' in item:
+        item['new'] = (date.today() - item['added']) < timedelta(days=30)
+    return item
+
 def parse_data(site):
     data = yaml.load(file(op.join(op.dirname(__file__), 'games.yaml')))
     site.clones = []
     site.reimplementations = []
     for item in data:
-        if 'added' in item:
-            item['added'] = datetime.strptime(item['added'], '%Y-%m-%d').date()
-            item['new'] = item['added'] < (datetime.now() - timedelta(days=30))
 
         if 'clones' in item:
-            site.clones.append((names(item), item['clones']))
+            site.clones.append(
+                (names(item), map(mark_new, item['clones'])))
         if 'reimplementations' in item:
-            site.reimplementations.append((names(item),
-                                           item['reimplementations']))
+            site.reimplementations.append(
+                (names(item), map(mark_new, item['reimplementations'])))
 
 
 def callback(site):
