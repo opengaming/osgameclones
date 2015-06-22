03aab4793a40fc66efdc82fcb823b3cfb7596d43
Alexander Solovyov <alexander@solovyov.net>
2015-06-22 22:46:11 +0300

    no agents, no whatever, just work

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,11 +2,9 @@
 
 set -ev
 
-test "${TRAVIS_PULL_REQUEST}" = "false" || exit 0
-
-openssl aes-256-cbc -K $encrypted_61da1a775a65_key -iv $encrypted_61da1a775a65_iv -in .travis/deploy_key.enc -out .travis/deploy_key -d
-chmod 600 .travis/deploy_key
-eval $(ssh-agent -s)
-ssh-add .travis/deploy_key
-git remote add maia dokku@maia.solovyov.net:osgc
-git push maia master
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+    openssl aes-256-cbc -K $encrypted_61da1a775a65_key -iv $encrypted_61da1a775a65_iv -in .travis/deploy_key.enc -out .travis/deploy_key -d
+    chmod 600 .travis/deploy_key
+    git remote add maia dokku@maia.solovyov.net:osgc
+    GIT_SSH_COMMAND="ssh -i .travis/deploy_key" git push maia master
+fi
