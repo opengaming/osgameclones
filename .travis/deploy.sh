835c519eff9e9d2e3dce0578088acb39229c867f
Alexander Solovyov <alexander@solovyov.net>
2015-06-22 21:36:42 +0300

    move deploy stuff to deploy.sh

---
+++ .travis/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -ev
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+    openssl aes-256-cbc -K $encrypted_61da1a775a65_key -iv $encrypted_61da1a775a65_iv -in .travis/deploy_key.enc -out .travis/deploy_key -d
+    chmod 600 .travis/deploy_key
+    ssh-add .travis/deploy_key
+    git remote add maia dokku@maia.solovyov.net:osgc
+    git push deploy
+fi
