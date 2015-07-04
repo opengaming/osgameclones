bb340515b5d278684f3db54ad5a9417193568b19
Alexander Solovyov <alexander@solovyov.net>
2015-07-04 14:45:23 +0300

    trying to debug deploy issues

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,6 +5,8 @@ set -ev
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     openssl aes-256-cbc -K $encrypted_61da1a775a65_key -iv $encrypted_61da1a775a65_iv -in .travis/deploy_key.enc -out .travis/deploy_key -d
     chmod 600 .travis/deploy_key
+    ls -l .travis/deploy_key
+    md5sum .travis/deploy_key
     git remote add maia dokku@maia.solovyov.net:osgc
-    GIT_SSH_COMMAND="ssh -i .travis/deploy_key" git push maia master
+    GIT_SSH_COMMAND="ssh -i .travis/deploy_key -o PasswordAuthentication=no" git push maia master
 fi
