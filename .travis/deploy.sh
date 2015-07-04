be7f3df8ef2851d36772ed1670507e525729d2b1
Alexander Solovyov <alexander@solovyov.net>
2015-07-04 15:26:05 +0300

    one-line push

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,7 +3,4 @@
 openssl aes-256-cbc -K $encrypted_61da1a775a65_key -iv $encrypted_61da1a775a65_iv -in .travis/deploy_key.enc -out .travis/deploy_key -d
 chmod 600 .travis/deploy_key
 
-cat .git/config
-git remote add maia dokku@maia.solovyov.net:osgc
-
-GIT_SSH=".travis/deploy.sh" git push maia master
+GIT_SSH=".travis/deploy.sh" git push dokku@maia.solovyov.net:osgc master
