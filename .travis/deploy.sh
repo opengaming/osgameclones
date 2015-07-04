09646a1a810f47ddbbcc94431fda52a48156089d
Alexander Solovyov <alexander@solovyov.net>
2015-07-04 15:21:03 +0300

    i do not understand how maia is already there

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-set -ev
-
 openssl aes-256-cbc -K $encrypted_61da1a775a65_key -iv $encrypted_61da1a775a65_iv -in .travis/deploy_key.enc -out .travis/deploy_key -d
 chmod 600 .travis/deploy_key
 
-git remote remove maia 2&>/dev/null || echo 'all clear'
+cat .git/config
 git remote add maia dokku@maia.solovyov.net:osgc
 
 GIT_SSH=".travis/deploy.sh" git push maia master
