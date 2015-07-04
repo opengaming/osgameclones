3a4c9d56512af248ee24bcd28e2761fa3ff6e467
Alexander Solovyov <alexander@solovyov.net>
2015-07-04 15:12:29 +0300

    ok, lets deploy a bit differently

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 set -ev
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    openssl aes-256-cbc -K $encrypted_61da1a775a65_key -iv $encrypted_61da1a775a65_iv -in .travis/deploy_key.enc -out .travis/deploy_key -d
-    chmod 600 .travis/deploy_key
-    git remote rm maia 2&>/dev/null || echo 'all clear'
-    git remote add maia dokku@maia.solovyov.net:osgc
-    GIT_SSH=".travis/deploy.sh" git push maia master
-fi
+openssl aes-256-cbc -K $encrypted_61da1a775a65_key -iv $encrypted_61da1a775a65_iv -in .travis/deploy_key.enc -out .travis/deploy_key -d
+chmod 600 .travis/deploy_key
+
+git remote remove maia 2&>/dev/null || echo 'all clear'
+git remote add maia dokku@maia.solovyov.net:osgc
+
+GIT_SSH=".travis/deploy.sh" git push maia master
