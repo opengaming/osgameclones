1b389eab5dae5fdb4c6bb3b03244f6d62fdc3738
Alexander Solovyov <alexander@solovyov.net>
2015-07-04 18:30:14 +0300

    omg i was so wrong

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+set -ev
+
 openssl aes-256-cbc -K $encrypted_61da1a775a65_key -iv $encrypted_61da1a775a65_iv -in .travis/deploy_key.enc -out .travis/deploy_key -d
 chmod 600 .travis/deploy_key
 
-GIT_SSH=".travis/deploy.sh" git push dokku@maia.solovyov.net:osgc master
+GIT_SSH=".travis/git-ssh.sh" git push dokku@maia.solovyov.net:osgc master
