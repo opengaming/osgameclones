2024daffac4fcb1168d007ce8522136ded1f9264
Alexander Solovyov <alexander@solovyov.net>
2018-12-18 09:07:11 +0200

    more newlines in danger output

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,4 @@
 const {danger, message} = require('danger')
 
-const modifiedMD = danger.git.modified_files.join("- ")
+const modifiedMD = danger.git.modified_files.join("\n- ")
 message("Changed Files in this PR: \n - " + modifiedMD)
