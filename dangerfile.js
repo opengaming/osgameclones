347b38504760b0e2dfd8e93e816d39dba13cc10b
Alexander Solovyov <alexander@solovyov.net>
2018-12-18 09:10:47 +0200

    markdown

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,4 @@
-const {danger, message} = require('danger')
+const {danger, markdown} = require('danger')
 
 const modifiedMD = danger.git.modified_files.join("\n- ")
-message("Changed Files in this PR: \n - " + modifiedMD)
+markdown("Changed Files in this PR:\n - " + modifiedMD)
