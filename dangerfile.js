53582452ab7cae952127381c1a62872d4bd7f8a4
Cong <congusbongus@gmail.com>
2019-01-18 14:04:10 +1100

    Danger put file information in collapsible area

--- dangerfile.js
+++ dangerfile.js
@@ -1,7 +1,23 @@
 const {danger, markdown} = require('danger')
 
 markdown("Hey there! Thanks for contributing a PR to osgameclones! 🎉")
-const modifiedMD = danger.git.modified_files.join("\n- ")
-if (modifiedMD.length > 0) {
-  markdown("Changed Files in this PR:\n - " + modifiedMD)
+
+// Information summary of files in the PR
+// For debug purposes only
+if (danger.git.modified_files.length || danger.git.created_files.length || danger.git.deleted_files.length) {
+  let changes = ""
+  
+  const getChanges = (title, files) => {
+    const md = files.join("\n- ")
+    if (md.length > 0) {
+      return title + ":\n - " + modifiedMD + "\n\n"
+    }
+    return ""
+  }
+
+  changes += getChanges("Changed", danger.git.modified_files)
+  changes += getChanges("Added", danger.git.created_files)
+  changes += getChanges("Deleted", danger.git.deleted_files)
+
+  markdown(`<details><summary>Files in PR...</summary><p>{changes}</p></details>`)
 }
