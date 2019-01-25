7fbb4ff8da1d1d8998ca737208326f0dce6425eb
Cong <congusbongus@gmail.com>
2019-01-25 13:03:30 +1100

    Update dangerfile.js

--- dangerfile.js
+++ dangerfile.js
@@ -1,10 +1,44 @@
-const {danger, markdown} = require('danger')
+const {danger, markdown, message} = require('danger')
 const yaml = require('js-yaml')
 const fs = require('fs')
 
 markdown("Hey there! Thanks for contributing a PR to osgameclones! 🎉")
 
 const isGame = game => /^games\/\w+\.yaml$/.test(game)
+
+const getGameChanges = files => {
+  Promise.all(files.filter(isGame).map(file => danger.git.diffForFile(file)))
+  .then(diffs => diffs.forEach(diff => {
+    const gamesBefore = yaml.safeLoad(diff.before)
+    const gamesAfter = yaml.safeLoad(diff.after)
+    const namesBefore = gamesBefore.map(game => game.name)
+    const namesAfter = gamesAfter.map(game => game.name)
+    let namesAdded = []
+    let namesChanged = []
+    let namesRemoved = []
+    gamesBefore.forEach(game => {
+      if (!namesAfter.includes(game.name)) {
+        namesRemoved.push(game.name)
+      } else if (namesAfter.includes(game.name) && !gamesAfter.includes(game)) {
+        namesChanged.push(game.name)
+      }
+    })
+    gamesAfter.forEach(game => {
+      if (!namesBefore.includes(game.name)) {
+        namesAdded.push(game.name)
+      }
+    })
+    if (namesAdded.length > 0) {
+      message(`Games added: ${danger.utils.sentence(namesAdded)} 🎊`)
+    }
+    if (namesChanged.length > 0) {
+      message(`Games updated: ${danger.utils.sentence(namesChanged)} 👏`)
+    }
+    if (namesRemoved.length > 0) {
+      message(`Games removed: ${danger.utils.sentence(namesRemoved)} 😿`)
+    }
+  }))
+}
 
 // Information summary of files in the PR
 // For debug purposes only
@@ -16,7 +50,6 @@ if (danger.git.modified_files.length || danger.git.created_files.length || dange
       if (isGame(file)) {
         const games = yaml.safeLoad(fs.readFileSync(file))
         const gamesList = games.map(game => `\n  - ${game.name}`)
-        danger.git.diffForFile(file).then(diff => markdown(`<!-- ${diff.diff} -->`))
         return `\n- 🎮 \`${file}\`${gamesList.join()})`
       }
       return `\n- \`${file}\``
