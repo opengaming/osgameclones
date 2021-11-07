2398d4c0bc27d97928bb6919d7d3e185bd79a9b7
Cong <congusbongus@gmail.com>
2021-11-07 13:31:12 +1100

    Update pipfile

--- .travis/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ev
-
-openssl aes-256-cbc -K $encrypted_61da1a775a65_key -iv $encrypted_61da1a775a65_iv -in .travis/deploy_key.enc -out .travis/deploy_key -d
-chmod 600 .travis/deploy_key
-
-GIT_SSH=".travis/git-ssh.sh" git push dokku@sirius.solovyov.net:osgc master
