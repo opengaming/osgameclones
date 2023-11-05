921517d0d079e7f0d2e501015b1728a3afab9840
Alex Palade <alexpalade@gmail.com>
2023-11-05 23:56:14 +0200

    Fix light theme preference is ignored (#2316)

--- static/dark-theme.js
+++ static/dark-theme.js
@@ -1,6 +1,6 @@
 const DARK_THEME_CLASS = 'dark-theme';
 const LIGHT_THEME_CLASS = 'light-theme';
-const DARK_THEME_KEY = 'startInDarkTheme';
+const THEME_KEY = 'theme';
 
 function setDarkTheme() {
   document.body.classList.remove(LIGHT_THEME_CLASS);
@@ -15,17 +15,17 @@ function setLightTheme() {
 function toggleDarkTheme() {
   if (document.body.classList.contains(DARK_THEME_CLASS)) {
     setLightTheme();
-    localStorage.setItem(DARK_THEME_KEY, 'false')
+    localStorage.setItem(THEME_KEY, 'light')
   } else {
     setDarkTheme();
-    localStorage.setItem(DARK_THEME_KEY, 'true')
+    localStorage.setItem(THEME_KEY, 'dark')
   }
 }
 
 // enable dark theme as soon as possible so first paint is already dark mode
 (function () {
   window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-    if (localStorage.getItem(DARK_THEME_KEY) === null) {
+    if (localStorage.getItem(THEME_KEY) === null) {
       if (event.matches) {
         setDarkTheme();
       } else {
@@ -33,8 +33,15 @@ function toggleDarkTheme() {
       }
     }
   });
-  if (localStorage.getItem(DARK_THEME_KEY) === 'true' || window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    setDarkTheme();
+
+  if (localStorage.getItem(THEME_KEY) === 'dark') {
+      setDarkTheme();
+  } else if (localStorage.getItem(THEME_KEY) === 'light') {
+      setLightTheme();
+  } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setDarkTheme();
+  } else {
+      setLightTheme();
   }
 })();
 
