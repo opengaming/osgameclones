33da57b81f984a30039c7a85fd7099ec909fc400
Cong <congusbongus@gmail.com>
2025-04-21 15:08:26 +1000

    Update pr_check.py

--- .github/workflows/pr_check.py
+++ .github/workflows/pr_check.py
@@ -15,7 +15,10 @@ repo = g.get_repo(GITHUB_REPOSITORY)
 pr = repo.get_pull(PR_NUMBER)
 
 # Get game changes
-patchset = PatchSet(httpx.get(pr.diff_url).read().decode())
+print("reading diff", pr.diff_url)
+diff = httpx.get(pr.diff_url).read().decode()
+print("diff", diff)
+patchset = PatchSet(diff)
 added = [patch.target_file for patch in patchset.added_files]
 modified = [patch.target_file for patch in patchset.modified_files]
 removed = [patch.target_file for patch in patchset.removed_files]
@@ -29,14 +32,17 @@ if removed:
 if change_comment:
     content += f"\n<!--{change_comment}-->"
 else:
-    content += f"\n<!--No changes found!-->"
+    content += "\n<!--No changes found!-->"
 
 # Update GitHub PR
 for c in pr.get_comments():
+    print("checking comment", c.user.login)
     if c.user.login == GITHUB_BOT_LOGIN:
+        print("found bot comment", c.body)
         comment = c
         if comment.body != content:
             comment.edit(content)
         break
 else:
+    print("bot comment not found")
     comment = pr.create_issue_comment(content)
